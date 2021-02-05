84499e44fa7db18227896f2a0eb8b817e05a9433
NielsPeeters96 <78353674+NielsPeeters96@users.noreply.github.com>
2021-02-05 12:11:58 +0100

    Issues

--- script.js
+++ script.js
@@ -12,38 +12,37 @@ app.appendChild(container)
 
 // Je fetched de data van de API en maakt er vervolgens jsonData van //
 async function fetchData() {
-const dataResponse = await fetch(`https://ghibliapi.herokuapp.com/films`)
-const jsonData = await dataResponse.json()
-console.log(jsonData);
-return jsonData;
+  const dataResponse = await fetch(`https://ghibliapi.herokuapp.com/films`)
+  const jsonData = await dataResponse.json()
+  return jsonData;
 } 
 
 // Dit zorgt ervoor dat je applicatie start, in de variable data stop je fetchdata en vervolgens render je de data bij renderMovies //
 async function init() {
-const data = await fetchData()
-renderMovies(data)
+  const data = await fetchData()
+  renderMovies(data)
 }
 
 function renderMovies(data) {
   data.forEach((movie) => {
     const card = document.createElement('div')
     card.setAttribute('class', 'card')
-
+    
     const h1 = document.createElement('h1')
     h1.textContent = movie.title
-
+    
     const p = document.createElement('p')
     movie.description = movie.description.substring(0, 300)
     p.textContent = `${movie.description}...`
-
+    
     const int = document.createElement('int')
     int.textContent = movie.release_date
-
-container.appendChild(card)
-card.appendChild(h1)
-card.appendChild(p)
-card.appendChild(int)
-})
-
+    
+    container.appendChild(card)
+    card.appendChild(h1)
+    card.appendChild(p)
+    card.appendChild(int)
+  })
+  
 }
 init()
