1e7ee7271c80adb7158fc846de09509e7b27ea9a
NielsPeeters96 <78353674+NielsPeeters96@users.noreply.github.com>
2021-02-26 09:54:14 +0100

    Last fixes

--- js/modules/fetching.js
+++ js/modules/fetching.js
@@ -1,9 +1,8 @@
 // Fetching data from API and return it as JSON
 async function fetchData() {
-    const dataResponse = await fetch(`https://ghibliapi.herokuapp.com/films`)
-    const jsonData = await dataResponse.json()
-    return jsonData;
-  } 
-  
-  // When starting the application making sure that everything is connected and using await fetchData for no errors //
-  export {fetchData};
+  const dataResponse = await fetch(`https://ghibliapi.herokuapp.com/films`)
+  const jsonData = await dataResponse.json()
+  return jsonData;
+} 
+
+export {fetchData};
